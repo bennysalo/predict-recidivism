32bd12d0455bdf6a696eec23d849f8d921096f06
bennysalo <benny.salo@abo.fi>
2018-04-23 15:14:34 +0200

    Document cross-validation results.

--- R/data_documentation.R
+++ R/data_documentation.R
@@ -16,5 +16,19 @@
 #' @source variable_table.csv compiled by Benny Salo
 "variable_table"
 
-
+#' Discrimination of models in training set.y
+#'
+#' .
+#' The 'values' element contains a data frame with 100 rows of the results in
+#' the 10 by 10 fold cross-validation. For each model the ROC(AUC),
+#' is recorded along with sensitivity and specificity for decision cutoff of 0.5.
+#' That gives us 24 model x 3 measures = 72 columns. With the resample numbers
+#' that is 73 columns.
+#'
+#' @format A list with 6 elements of class 'resamples' created by
+#' 'caret::resamples'
+#' @seealso caret::resamples
+#'
+#' @source Scripts for how the results were created are stored in /data-raw
+"model_AUCs_training_set"
 
