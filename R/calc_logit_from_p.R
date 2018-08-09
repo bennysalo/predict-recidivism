7e2fceae6f3ea2ecd0472579fec7b8f316fd4860
bennysalo <benny.salo@abo.fi>
2018-08-09 09:50:21 +0200

    Edit and document function.

--- R/calc_logit_from_p.R
+++ R/calc_logit_from_p.R
@@ -1,31 +1,34 @@
 
 #' Caclulate logit (log odds) from probability
 #'
-#' @param probability A number between 0 and 1.
-#' @param offset_for_01 A number to add to 0 or subtract from 1 to make
+#' @param probability A numerical vector (or single number) with probabilities
+#'   between 0 and 1.
+#' @param offset_for_01 A decimal number to add to probability 0 or subtract from 1 to make
 #'   calculation of logit possible.
 #'
-#' @return Logit (log odds)
+#' @return A numerical vector (or single number) representing the logit (log
+#'   odds).
 #'
 #' @examples
 #' calc_logit_from_p(0.2)
+#'
+#' probabilities <- c(0.00, 0.07, 0.12,0.74, 1.00)
+#' calc_logit_from_p(probabilities, offset_for_01 = 0.001)
+#'
 calc_logit_from_p <- function(probability, offset_for_01 = NULL) {
 
 # Use offset if provided
-  if(!is.null(offset_for_01) & probability == 0) {
-    probability <- offset_for_01
-  }
-
-  if(!is.null(offset_for_01) & probability == 1) {
-    probability <- 1 - offset_for_01
+  if(!is.null(offset_for_01)) {
+    probability[probability == 0] <- 0 + offset_for_01
+    probability[probability == 1] <- 1 - offset_for_01
   }
 
 # Inform that logits cannot be caluclated when p = 0 or 1
-  if(probability %in% c(0,1)) {
+  if(any(probability %in% c(0,1))) {
     stop("Probability is 0 or 1, for which logit cannot be calculated",
          .call = FALSE)
-  } else if (probability > 1 | probability < 0) {
-    stop("Probability is not between 0 and 1",
+  } else if (any(probability > 1 | probability < 0)) {
+    stop("One or more probabilities are not between 0 and 1",
          .call = FALSE)
   }
   logit <- log(probability / (1-probability))
--- tests/testthat/test_calc_logit_from_p.R
+++ tests/testthat/test_calc_logit_from_p.R
@@ -13,15 +13,23 @@ test_that("offset is added correctly to 0 and 1", {
   expect_equal(calc_logit_from_p(1, 0.0001), log(0.9999 / 0.0001))
 })
 
+test_that("offset is used with all 0's and 1's in a vector", {
+  expect_equal(calc_logit_from_p(c(0, 0.5, 1), offset_for_01 = 0.01),
+                                 c(log(0.01 / 0.99), log(0.5 / 0.5), log(0.99 /0.01)))
+
+})
+
 test_that("offset is not added when probability is not 0 or 1", {
   expect_equal(calc_logit_from_p(0.5, 0.001), log(0.5 / 0.5))
   expect_equal(calc_logit_from_p(0.3, 0.1), log(0.3 / 0.7))
 })
 
-test_that("logit of 0 and 1 fails with no offset", {
+test_that("logit of 0 and 1 fails when there is no offset", {
   expect_error(calc_logit_from_p(0),
                "Probability is 0 or 1, for which logit cannot be calculated")
   expect_error(calc_logit_from_p(1),
                "Probability is 0 or 1, for which logit cannot be calculated")
 })
+
+
 
